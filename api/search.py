7929a37db979f5b374c4d22533b5576e3837cd48
Karannshah1 <80153010+Karannshah1@users.noreply.github.com>
2025-08-08 09:36:07 +0530

    Update search.py

--- api/search.py
+++ api/search.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 import yt_dlp
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="../templates")
 
 YTDL_OPTS = {
     'quiet': True,
